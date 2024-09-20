bae53dfcfd815a35cc5786a0ff57c7d1399e2b81
cocoin123 <166143145+cocoin123@users.noreply.github.com>
2024-09-20 17:43:06 +0500

    Add files via upload

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='CryptoHelper',
-    version='0.1',
+    name='cryptohelper',
+    version='0.2',
     packages=find_packages(),
     description='Данный хелпер облегчит вам жизнь для использования AioCryptoPay',
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     author='cocoin',
-    author_email='test@exemple.com',
-    url='https://github.com/soon',  # или другая ссылка
+    author_email='soon',
+    url='https://github.com/cocoin123/CryptoHelper',  # или другая ссылка
     classifiers=[
         'Programming Language :: Python :: 3',
         'License :: OSI Approved :: MIT License',
